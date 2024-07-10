69c16520ac16f5cfad47746971ffd4c9c7e0cf26
nakamura <hrm.nkmr04@gmail.com>
2024-07-11 07:54:00 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var boolean = false;
+if (boolean) {
+	console.log("trueの処理");
+	console.log(boolean);
+} else {
+	console.log("falseの処理");
+	console.log(boolean);
+};
+/* elseは　後ろの「: (コロン)」のつけ忘れに注意 */
+/*　else文に含まれる処理は、インデントして記述する。処理が複数行ある場合も、全てインデントする。インデントしていない部分はelseに含まれない　*/
+/* elseの条件が正しく反映されているか確認する*/
+var num = 100
+if (num === 90) {
+	console.log(num);
+} else {
+	console.log("100");
+}
+var num = 70;
+if (num > 80) {
+	console.log("numは80より大きいです。");
+} else {
+	console.log("numは80以下です。");
+}
