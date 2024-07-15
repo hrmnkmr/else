89bdc11ac3fb1f1ce7ccf3dce37133960a1b22bc
nakamura <hrm.nkmr04@gmail.com>
2024-07-16 06:51:49 +0900

    second commit

--- main.js
+++ main.js
@@ -11,13 +11,17 @@ if (boolean) {
 /* elseの条件が正しく反映されているか確認する*/
 var num = 100
 if (num === 90) {
+	console.log("falseの処理");
 	console.log(num);
 } else {
+	console.log("trueの処理");
 	console.log("100");
 }
 var num = 70;
 if (num > 80) {
+	console.log("falseの処理");
 	console.log("numは80より大きいです。");
 } else {
+	console.log("trueの処理");
 	console.log("numは80以下です。");
 }
